36944512d96089500c26b728cc5fc5c8369abec5
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-01-17 17:06:29 -0800

    Add placeholder home content

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,82 @@
+import styles from "./Home.module.css";
+
 function Home() {
-  return('home');
+  return (
+    <div className={styles.container}>
+      <section>
+        <h2>Home Page</h2>
+        <p>
+          Asperiores at qui reprehenderit voluptas sed et. Quod voluptatem eius
+          eum dolorum molestiae quos quas. Vero enim consectetur nihil minima.
+        </p>
+        <p>
+          Aut dolorem et ut est totam omnis. Facilis et eos expedita. Consequatur
+          maiores magni dolorem. Est non asperiores blanditiis perferendis.
+        </p>
+        <p>
+          Corrupti quas et necessitatibus. Aut ab sit deserunt placeat aut iste
+          nemo. Aspernatur dignissimos accusamus qui aut nam temporibus
+          voluptatibus. Ratione velit quidem esse ut vero fugiat. Aliquam
+          accusantium et omnis sit veritatis sit et accusamus. Eum esse unde
+          recusandae aut aut harum.
+        </p>
+        <p>
+          Velit eligendi omnis aliquid. Sapiente in dolorum commodi commodi. Sed
+          quaerat iusto deserunt at repellendus. Ullam totam libero est excepturi.
+          Voluptates unde ut minima qui. Nihil exercitationem provident excepturi
+          qui iure officia ex itaque.
+        </p>
+      </section>
+      <section>
+        <h2>Extra Information</h2>
+        <p>
+          Asperiores at qui reprehenderit voluptas sed et. Quod voluptatem eius
+          eum dolorum molestiae quos quas. Vero enim consectetur nihil minima.
+        </p>
+        <p>
+          Aut dolorem et ut est totam omnis. Facilis et eos expedita. Consequatur
+          maiores magni dolorem. Est non asperiores blanditiis perferendis.
+        </p>
+        <p>
+          Corrupti quas et necessitatibus. Aut ab sit deserunt placeat aut iste
+          nemo. Aspernatur dignissimos accusamus qui aut nam temporibus
+          voluptatibus. Ratione velit quidem esse ut vero fugiat. Aliquam
+          accusantium et omnis sit veritatis sit et accusamus. Eum esse unde
+          recusandae aut aut harum.
+        </p>
+        <p>
+          Velit eligendi omnis aliquid. Sapiente in dolorum commodi commodi. Sed
+          quaerat iusto deserunt at repellendus. Ullam totam libero est excepturi.
+          Voluptates unde ut minima qui. Nihil exercitationem provident excepturi
+          qui iure officia ex itaque.
+        </p>
+      </section>
+      <section>
+        <h2>Conclusion</h2>
+        <p>
+          Asperiores at qui reprehenderit voluptas sed et. Quod voluptatem eius
+          eum dolorum molestiae quos quas. Vero enim consectetur nihil minima.
+        </p>
+        <p>
+          Aut dolorem et ut est totam omnis. Facilis et eos expedita. Consequatur
+          maiores magni dolorem. Est non asperiores blanditiis perferendis.
+        </p>
+        <p>
+          Corrupti quas et necessitatibus. Aut ab sit deserunt placeat aut iste
+          nemo. Aspernatur dignissimos accusamus qui aut nam temporibus
+          voluptatibus. Ratione velit quidem esse ut vero fugiat. Aliquam
+          accusantium et omnis sit veritatis sit et accusamus. Eum esse unde
+          recusandae aut aut harum.
+        </p>
+        <p>
+          Velit eligendi omnis aliquid. Sapiente in dolorum commodi commodi. Sed
+          quaerat iusto deserunt at repellendus. Ullam totam libero est excepturi.
+          Voluptates unde ut minima qui. Nihil exercitationem provident excepturi
+          qui iure officia ex itaque.
+        </p>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
